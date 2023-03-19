a2c589b0b3d2b640e75804d19c4670d5dcaed225
takaka-t <>
2023-03-19 14:52:23 +0900

    開発環境の設定 VSCode,Docker,Vue

--- article-development/vite.config.ts
+++ article-development/vite.config.ts
@@ -1,14 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // ホットリロード
+  server: {
+    watch: { usePolling: true },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  // ビルド時の各ファイルのサイズ制限(かな?)
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+});
