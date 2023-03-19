052d257da30ec89ca7558050bb564da72d29016c
takaka-t <>
2023-03-19 15:11:25 +0900

    ライブラリ設定 vuetify,lodash,pinia-plugin-persistedstate エラーハンドラ設定 errorHandler

--- article-development/src/main.ts
+++ article-development/src/main.ts
@@ -1,14 +1,64 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import App from "@/App.vue";
+import "@/assets/main.css";
 
-import App from './App.vue'
-import router from './router'
+// Router
+import router from "@/router";
 
-import './assets/main.css'
+// Pinia
+import { createPinia } from "pinia";
+import { createPersistedState } from "pinia-plugin-persistedstate";
+const pinia = createPinia();
+pinia.use(createPersistedState());
 
-const app = createApp(App)
+// Vuetify
+import "vuetify/styles";
+import { createVuetify, type ThemeDefinition } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 
-app.use(createPinia())
-app.use(router)
+const mainTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: "#FFFFFF",
+    surface: "#FFFFFF",
+    primary: "#E85D3A",
+    secondary: "#bc867e",
+    success: "#F7B3A2", //pink
+    accent: "#333",
+    info: "#333",
+    warning: "#ff0000",
+    error: "#ff0000",
+  },
+};
 
-app.mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: "mainTheme",
+    themes: {
+      mainTheme,
+    },
+  },
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
+
+// errorHandler
+import { errorHandler } from "@/errorHandler";
+
+const app = createApp(App);
+
+app.use(router);
+app.use(pinia);
+app.use(vuetify);
+app.use(errorHandler);
+
+app.mount("#app");
